adaf78eaab33925f24bf9df0efd795ea68c9e1f5
charivna <124678194+charivna@users.noreply.github.com>
2023-08-05 18:16:29 +0200

    id

--- src/js/book-by-category.js
+++ src/js/book-by-category.js
@@ -14,14 +14,14 @@ function onClick(evt) {
     
    
     getBookByCategory(selectedCategory).then(data =>
-      
+       
         createCategoryMarkup(data,selectedCategory))
 }
 
 function createCategoryMarkup(data,selectedCategory) {
     const titleSection = `<h2 class="js-category-title">${selectedCategory}</h2>`;
     console.log(titleSection);
-  const bookByGenre = data.map(({ book_image, title, contributor }) => `<li class = "book-by-genre">
+  const bookByGenre = data.map(({ book_image, title, contributor, _id }) => `<li class = "book-by-genre" id=${_id}>
             <img class ="img_book" src="${book_image}" alt="">
             <h3 class ="book-name">${title}</h3>
             <p class ="contributor">${contributor}</p>
