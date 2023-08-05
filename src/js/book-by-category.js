a9435f531ec98df3341085d763a3c39d89acd94b
charivna <124678194+charivna@users.noreply.github.com>
2023-08-05 15:17:34 +0200

    push js

--- src/js/book-by-category.js
+++ src/js/book-by-category.js
@@ -2,7 +2,7 @@ import { getBookByCategory } from "./api";
 
 const refs = {
     categoriesContEl: document.querySelector('.category__list'),
-    bookEl: document.querySelector('.book_container')
+    bookEl: document.querySelector('.book_cards')
 }
 
 refs.categoriesContEl.addEventListener('click', onClick)
@@ -11,20 +11,21 @@ function onClick(evt) {
 
     const selectedCategory = evt.target.textContent;
     
-    
+   
     getBookByCategory(selectedCategory).then(data =>
       
-        createCategoryMarkup(data))
+        createCategoryMarkup(data,selectedCategory))
 }
 
-function createCategoryMarkup(data) {
- 
+function createCategoryMarkup(data,selectedCategory) {
+    const titleSection = `<h2 class="js-category-title">${selectedCategory}</h2>`;
+    console.log(titleSection);
   const bookByGenre = data.map(({ book_image, title, contributor }) => `<li>
             <img src="${book_image}" alt="">
             <h3>${title}</h3>
             <p>${contributor}</p>
         </li>`).join(' ')
-  refs.bookEl.innerHTML = bookByGenre;
+    refs.bookEl.innerHTML = titleSection + bookByGenre;
 }
 
 
