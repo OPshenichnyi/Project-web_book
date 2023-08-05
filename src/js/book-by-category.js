aaaad54f39e6b5ba6c3fd4609963a523eec3cc38
charivna <124678194+charivna@users.noreply.github.com>
2023-08-05 16:44:31 +0200

    push

--- src/js/book-by-category.js
+++ src/js/book-by-category.js
@@ -20,12 +20,13 @@ function onClick(evt) {
 function createCategoryMarkup(data,selectedCategory) {
     const titleSection = `<h2 class="js-category-title">${selectedCategory}</h2>`;
     console.log(titleSection);
-  const bookByGenre = data.map(({ book_image, title, contributor }) => `<li>
-            <img src="${book_image}" alt="">
-            <h3>${title}</h3>
-            <p>${contributor}</p>
+  const bookByGenre = data.map(({ book_image, title, contributor }) => `<li class = "book-by-genre">
+            <img class ="img_book" src="${book_image}" alt="">
+            <h3 class ="book-name">${title}</h3>
+            <p class ="contributor">${contributor}</p>
         </li>`).join(' ')
-    refs.bookEl.innerHTML = titleSection + bookByGenre;
+    const containerForBooks = `<ul class="js-cont-for-books">${bookByGenre}</ul>`
+    refs.bookEl.innerHTML = titleSection + containerForBooks;
 }
 
 
