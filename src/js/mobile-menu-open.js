0482d3ccba7d7a210659af3f84800aa58cd8c350
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-08-08 00:46:18 +0300

    push-branch

--- src/js/mobile-menu-open.js
+++ src/js/mobile-menu-open.js
@@ -2,17 +2,44 @@
 const hiddebBurgMenu = document.querySelector('.conteiner-hidden');
  const burgerBtn = document.querySelector('.burger-button');
 
-burgerBtn.addEventListener('click', function (e) {
+
+burgerBtn.addEventListener('click', function () {
     
     if (hiddebBurgMenu.hasAttribute('hidden')) {
-        console.log(hiddebBurgMenu.hidden = false);
+        hiddebBurgMenu.hidden = false;
+
         
+
+
+
+burgerBtn.innerHTML=`
+<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none">
+<path stroke="#111" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M16 2 2 16M2 2l14 14"/>
+</svg>`;
 
         return;        
     }
-              console.log(hiddebBurgMenu.hidden = true);
 
-     });
+    burgerBtn.innerHTML=`
+    <svg
+    class ='js-icon-burger'
+      xmlns="http://www.w3.org/2000/svg"
+      width="28"
+      height="28"
+      fill="none"
+    >
+      <path
+        stroke="#111"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M19.833 11.667H3.5M24.5 7h-21M24.5 16.333h-21M19.833 21H3.5"
+      />
+    </svg>`;
+
+        hiddebBurgMenu.hidden = true;
+
+     })
 
 
 
