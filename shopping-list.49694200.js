c04fb17943f6058a5cde6652b388a466adc7a5ed
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2023-08-07 19:05:37 +0000

    Deploying to gh-pages from @ OPshenichnyi/Project-web_book@0e3242d60f7ab63fc309081a2af2390c965d3ec8 🚀

---
+++ shopping-list.49694200.js
@@ -0,0 +1,2 @@
+var switcher=document.querySelector("#switch");function switchTheme(){var t,e=document.documentElement;t="light"===e.getAttribute("data-theme")?"dark":"light",e.setAttribute("data-theme",t)}switcher.addEventListener("click",switchTheme);
+//# sourceMappingURL=shopping-list.49694200.js.map
