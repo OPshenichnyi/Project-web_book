82d2d756540b3661f90132c5394f53576f164f34
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-08-08 10:55:41 +0300

    shoping-list-burger menu

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,5 +1,4 @@
 
-
 // Получаем текущий путь страницы (например, /about.html)
 const currentPath = window.location.pathname;
 
@@ -8,12 +7,7 @@ const menuLinks = document.querySelectorAll('.burger-mobile-header-link');
 const currentHome = document.querySelector('.burger-header-link-first')
 // Проходим по ссылкам и добавляем класс "active" к текущей странице
 menuLinks.forEach(link => {
+    
     currentHome.classList.add('active');
-    if (link.getAttribute('href') === currentPath) {
-        currentHome.classList.remove('active');
-    link.classList.add('active');
-    } else {
-        //  currentHome.classList.remove('active');
-    link.classList.remove('active');
-  }
+
 });
