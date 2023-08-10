33407a4633b50c2fa40fd2a38b74923b670baf83
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2023-08-11 01:30:27 +0300

    Fix bag

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import './js/books';
 import './js/book-by-category';
 import './js/switcher';
 import './js/scroll-up';
-import './js/shopping-list';
 import './js/burger';
 import './js/mobile-menu-open';
 import './js/modal-window'
-import './js/shopping-list'
+
 
 
